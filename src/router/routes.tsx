dc5d4129a7eaa440a8e96bda1efd70982a8ff4ab
NingJjwo <ixxjuandavidgxxi@gmail.com>
2026-08-04 21:04:04 -0500

    feat: add docs route

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
+import Navbar from '../components/NavBar'
 import HomePage from '../pages/HomePage'
+import DocsPage from '../pages/DocsPage'
 
 export const router = createBrowserRouter([
   {
-    
     path: '/',
-    element: <HomePage/>,
+    element: <Navbar />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'docs', element: <DocsPage /> },
+    ],
   },
 ])
